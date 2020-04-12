1eb31f2793230557f46205aa37e19ba7106fe216
Liel Bach <ybach123@gmail.com>
2020-04-12 09:25:21 +0300

    First implementation of simple 5 layer nn

--- blood_samples.py
+++ blood_samples.py
@@ -1,9 +1,23 @@
 from blood_samples_reader import read_samples
-from sklearn.cluster import MeanShift
-from plotly.express import scatter
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.losses import mean_squared_error
+from keras.activations import relu
 
-blood_samples = read_samples('blood-samples.csv')
-bloods_samples_predictions = MeanShift().fit_predict(X=blood_samples)
+blood_samples, ages = read_samples()
 
-scatter(blood_samples, x=bloods_samples_predictions).show()
+inputs = Input(shape=(20,))
 
+layer_1 = Dense(512, activation=relu)(inputs)
+layer_2 = Dense(4096, activation=relu)(layer_1)
+layer_3 = Dense(4096, activation=relu)(layer_2)
+layer_4 = Dense(4096, activation=relu)(layer_3)
+layer_5 = Dense(128, activation=relu)(layer_4)
+
+predictions = Dense(1)(layer_5)
+
+model = Model(inputs=inputs, outputs=predictions)
+model.compile(optimizer=SGD(lr=0.000001),
+              loss=mean_squared_error)
+model.fit(blood_samples, ages, validation_split=0.2, epochs=600, verbose=2, batch_size=6000)
--- blood_samples_reader.py
+++ blood_samples_reader.py
@@ -1,5 +1,18 @@
-from numpy import array
 from pandas import read_csv, to_numeric
 
-def read_samples(file_name):
-    return read_csv(file_name, index_col='SEQN', skip_blank_lines=True, na_values=" ").dropna().apply(to_numeric)
+def read_samples():
+    year_column_name = 'RIDAGEYR'
+    index_column_name = 'SEQN'
+
+    blood_samples = read_csv('blood_samples.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")\
+        .dropna()\
+        .apply(to_numeric)
+    samples_demographic_data =  read_csv('demographic_data.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")\
+        .filter([index_column_name, year_column_name])\
+        .dropna()\
+        .apply(to_numeric)
+
+    complete_data_samples = blood_samples.join(samples_demographic_data)
+
+    return (complete_data_samples.drop(year_column_name, axis='columns').to_numpy(),
+            complete_data_samples.loc[:, year_column_name].to_numpy())
