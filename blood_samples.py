b07ab42e1b9a95aea68f9ee326735f05e4a29839
Liel Bach <ybach123@gmail.com>
2020-04-12 19:53:16 +0300

    Interesting revision

--- blood_samples.py
+++ blood_samples.py
@@ -4,20 +4,21 @@ from keras.models import Model
 from keras.optimizers import SGD
 from keras.losses import mean_squared_error
 from keras.activations import relu
+from sklearn.preprocessing import StandardScaler
 
 blood_samples, ages = read_samples()
+scaled_blood_samples = StandardScaler().fit_transform(blood_samples)
 
-inputs = Input(shape=(20,))
+inputs = Input(shape=(scaled_blood_samples.shape[1],))
 
-layer_1 = Dense(512, activation=relu)(inputs)
-layer_2 = Dense(4096, activation=relu)(layer_1)
-layer_3 = Dense(4096, activation=relu)(layer_2)
-layer_4 = Dense(4096, activation=relu)(layer_3)
-layer_5 = Dense(128, activation=relu)(layer_4)
+layer_1 = Dense(4096, activation=relu)(inputs)
+layer_2 = Dense(2048, activation=relu)(layer_1)
+layer_3 = Dense(512, activation=relu)(layer_2)
 
-predictions = Dense(1)(layer_5)
+predictions = Dense(1)(layer_3)
 
 model = Model(inputs=inputs, outputs=predictions)
-model.compile(optimizer=SGD(lr=0.000001),
+model.compile(optimizer=SGD(lr=0.00001),
               loss=mean_squared_error)
-model.fit(blood_samples, ages, validation_split=0.2, epochs=600, verbose=2, batch_size=6000)
+model.fit(scaled_blood_samples, ages, validation_split=0.2, epochs=10000,
+    verbose=2, batch_size=1024)
--- blood_samples_reader.py
+++ blood_samples_reader.py
@@ -4,15 +4,18 @@ def read_samples():
     year_column_name = 'RIDAGEYR'
     index_column_name = 'SEQN'
 
-    blood_samples = read_csv('blood_samples.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")\
-        .dropna()\
-        .apply(to_numeric)
+    blood_samples = read_csv('blood_samples.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")
     samples_demographic_data =  read_csv('demographic_data.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")\
-        .filter([index_column_name, year_column_name])\
-        .dropna()\
-        .apply(to_numeric)
+        .filter([index_column_name, year_column_name])
+    cholestrol = read_csv('chol.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")
+    body_measures = read_csv('body_measures.csv', index_col=index_column_name, skip_blank_lines=True, na_values=" ")\
+        .fillna(0)
 
-    complete_data_samples = blood_samples.join(samples_demographic_data)
+    complete_data_samples = blood_samples.join(samples_demographic_data)\
+                                        .join(cholestrol)\
+                                        .join(body_measures)\
+                                        .dropna()\
+                                        .apply(to_numeric)
 
     return (complete_data_samples.drop(year_column_name, axis='columns').to_numpy(),
             complete_data_samples.loc[:, year_column_name].to_numpy())
