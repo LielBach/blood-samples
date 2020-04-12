20be997d5ae90570f9a9dcccb1a300aeda9e8d39
Liel Bach <ybach123@gmail.com>
2020-04-13 00:02:49 +0300

    Precision of 10 years

--- blood_samples.py
+++ blood_samples.py
@@ -11,7 +11,7 @@ scaled_blood_samples = StandardScaler().fit_transform(blood_samples)
 
 inputs = Input(shape=(scaled_blood_samples.shape[1],))
 
-layer_1 = Dense(4096, activation=relu)(inputs)
+layer_1 = Dense(8012, activation=relu)(inputs)
 layer_2 = Dense(2048, activation=relu)(layer_1)
 layer_3 = Dense(512, activation=relu)(layer_2)
 
@@ -21,4 +21,4 @@ model = Model(inputs=inputs, outputs=predictions)
 model.compile(optimizer=SGD(lr=0.00001),
               loss=mean_squared_error)
 model.fit(scaled_blood_samples, ages, validation_split=0.2, epochs=10000,
-    verbose=2, batch_size=1024)
+    verbose=2, batch_size=512)
