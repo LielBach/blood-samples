e043bdf2c80935e0cef1ee5e0bb4d7c4258760e5
Liel Bach <ybach123@gmail.com>
2020-04-13 18:02:33 +0300

    Improved learning rate of SGD. Now running on plaidml.

--- blood_samples.py
+++ blood_samples.py
@@ -6,6 +6,7 @@ from keras.losses import mean_squared_error
 from keras.activations import relu
 from sklearn.preprocessing import StandardScaler
 
+
 blood_samples, ages = read_samples()
 scaled_blood_samples = StandardScaler().fit_transform(blood_samples)
 
@@ -20,5 +21,6 @@ predictions = Dense(1)(layer_3)
 model = Model(inputs=inputs, outputs=predictions)
 model.compile(optimizer=SGD(lr=0.00001),
               loss=mean_squared_error)
-model.fit(scaled_blood_samples, ages, validation_split=0.2, epochs=10000,
-    verbose=2, batch_size=512)
+model.fit(scaled_blood_samples, ages, validation_split=0.2, epochs=15000,
+    verbose=2, batch_size=6000)
+model.save('blood_samples_model')
