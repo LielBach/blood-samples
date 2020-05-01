6a43fab2a842b54df177b96fd2ff8c44b86ad677
Liel Bach <ybach123@gmail.com>
2020-05-01 09:28:52 +0300

    Fixed redundant model loading

--- blood_samples.py
+++ blood_samples.py
@@ -1,5 +1,5 @@
 from blood_samples_reader import read_samples
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Add
 from keras.models import Model, load_model
 from keras.optimizers import SGD
 from keras.losses import mean_squared_error
@@ -14,13 +14,13 @@ inputs = Input(shape=(scaled_blood_samples.shape[1],))
 layer_1 = Dense(8012, activation=relu)(inputs)
 layer_2 = Dense(2048, activation=relu)(layer_1)
 layer_3 = Dense(512, activation=relu)(layer_2)
+layer_4 = Dense(scaled_blood_samples.shape[1], activation=relu)(layer_3)
 
-predictions = Dense(1)(layer_3)
+predictions = Dense(1)(Add()([layer_4, inputs]))
 
 model = Model(inputs=inputs, outputs=predictions)
 model.compile(optimizer=SGD(lr=0.00001),
               loss=mean_squared_error)
-model = load_model('blood_samples_model')
 model.fit(scaled_blood_samples, ages, validation_split=0.2, epochs=30000,
     verbose=2, batch_size=6000)
 model.save('blood_samples_model')
